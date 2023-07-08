6b9b22fb0dbe8bef7667fdba559affcfb23c104c
yevhenii.plisko <yevhenii.plisko.contractor@fujifilm.com>
2023-07-08 14:04:37 +0300

    ToDoList initial commit

---
+++ WpfApp1/NewWindowViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Common;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WpfApp1
+{
+    public partial class NewWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private bool showAll;
+
+        [ObservableProperty]
+        private bool showComleted;
+
+        [ObservableProperty]
+        private bool showToDoOnly;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(RemoveToDoItemCommand))]
+        private ToDoItem? selectedItem;
+
+        [ObservableProperty]
+        private ObservableCollection<ToDoItem>? toDoListItems;
+
+        [ObservableProperty]
+        private ObservableCollection<ToDoItem>? toDoListSelectedItems;
+
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(AddNewToDoItemCommand))]
+        private string? newToDoText;
+
+        public RelayCommand AddNewToDoItemCommand { get; }
+        public RelayCommand RemoveToDoItemCommand { get; }
+        public RelayCommand UpdateSelectedListCommand { get; }
+
+        public NewWindowViewModel()
+        {
+            ShowAll = true;
+            ToDoListItems = new ObservableCollection<ToDoItem>();
+            ToDoListItems?.Add(new ToDoItem("Comleted", true));
+            AddNewToDoItemCommand = new RelayCommand(AddNewToDoItem, () => !string.IsNullOrEmpty(NewToDoText));
+            RemoveToDoItemCommand = new RelayCommand(RemoveToDoItem, () => SelectedItem is not null);
+            UpdateSelectedListCommand = new RelayCommand(UpdateSelectedList);
+        }
+
+        private void AddNewToDoItem()
+        {
+            ToDoListItems?.Add(new ToDoItem(NewToDoText));
+        }
+
+        private void RemoveToDoItem()
+        {
+            ToDoListItems?.Remove(SelectedItem);
+        }
+
+        private void UpdateSelectedList()
+        {
+            ToDoListSelectedItems = ToDoListItems;
+            if (!ShowAll)
+            {
+                var filtredList = ToDoListSelectedItems?.Where(x => x.IsCompleted == ShowComleted);
+                ToDoListSelectedItems = filtredList == null ? new ObservableCollection<ToDoItem>() : new ObservableCollection<ToDoItem>(filtredList);
+            }
+        }
+        partial void OnToDoListItemsChanged(ObservableCollection<ToDoItem>? value)
+        {
+            UpdateSelectedList();
+        }
+    }
+}
---
+++ WpfApp1/ToDoItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WpfApp1
+{
+    public partial class ToDoItem : ObservableObject
+    {
+        [ObservableProperty]
+        private string? name;
+
+        [ObservableProperty]
+        private bool? isCompleted;
+
+        public ToDoItem(string? _name, bool? _isCompleted = false) 
+        {
+            Name = _name;
+            IsCompleted = _isCompleted; 
+        }
+    }
+}
